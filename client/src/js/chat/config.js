aed5eacedcf22854b5196499d37fbbe92deac028
A.Skriabin <A.Skriabin@asntl.ru>
2023-06-26 12:55:55 +0300

    vercel config

--- client/src/js/chat/config.js
+++ client/src/js/chat/config.js
@@ -1,5 +1,7 @@
 const URIlocal = 'http://localhost:3000'
-const URIprod = 'https://chat-noname.herokuapp.com'
+// const URIprod = 'https://chat-noname.herokuapp.com'
+const URIprod = 'https://anonymous-chat-weld.vercel.app'
+
 const EVENT = {
   CHAT_MSG: 'CHAT_MSG',
   EDIT_MSG: 'EDIT_MSG',
